f327b803c438b8d6093310533cd19290e5d33f1f
Risachee <kaleriya.sych@mail.ru>
2026-04-24 10:20:16 +0300

    refactor(api): optimize project-task relations and query performance

--- projects/views.py
+++ projects/views.py
@@ -2,9 +2,23 @@ from rest_framework.viewsets import ModelViewSet
 from .models import Project
 from .serializers import ProjectSerializer
 
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import PermissionDenied
+
 class ProjectViewSet(ModelViewSet):
     serializer_class = ProjectSerializer
     queryset = Project.objects.all()
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+    def get_queryset(self):
+        return Project.objects.filter(owner=self.request.user)
+
+class ProjectViewSet(ModelViewSet):
+    permission_classes = [IsAuthenticated]
+
+    def perform_update(self, serializer):
+        if serializer.instance.owner != self.request.user:
+            raise PermissionDenied()
+        serializer.save()
