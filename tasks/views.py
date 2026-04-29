8778f859477aa97a521748ebc59b5906b21898c5
Risachee <valeriano4kas@gmail.com>
2026-04-29 12:04:53 +0300

    feat: add filtering and search for tasks

--- tasks/views.py
+++ tasks/views.py
@@ -2,11 +2,16 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from .models import Task
 from .serializers import TaskSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 
 
 class TaskViewSet(ModelViewSet):
     serializer_class = TaskSerializer
     permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ["status", "project"]
+    search_fields = ["title"]
 
     ordering = ["order"]  # сортировка
 
