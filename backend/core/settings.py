866bcc3458c21b179a4171b419376a6353285fac
Risachee <valeriano4kas@gmail.com>
2026-05-21 21:52:39 +0300

    chore: add CORS configuration

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -47,9 +47,12 @@ INSTALLED_APPS = [
     'comments',
     'guests',
     'clients',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -57,6 +60,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # Vite
+    "http://127.0.0.1:5173",
 ]
 
 ROOT_URLCONF = 'core.urls'
